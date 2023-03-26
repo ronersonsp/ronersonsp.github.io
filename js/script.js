e979d5f8ff070f49358e6e136d95bcfa0ded55a3
ronersonsp <duelgd@gmail.com>
2023-03-26 15:34:10 -0300

    adicionado animação

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+import Scroll from './modules/scroll-suave.js';
+const scroll = new Scroll('[data-menu="suave"] a[href^="#"]');
+scroll.init();
+
 if (window.matchMedia('(max-width:500px)').matches) {
   document.getElementById('agua').remove();
 } 
